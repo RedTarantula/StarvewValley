819085892535ec222894b19eff8bb2cdb3bc3a43
RedTarantula <joaopedrothmann@hotmail.com>
2019-06-16 15:35:00 -0300

    first commit

---
+++ Mod/modinfo.lua
@@ -0,0 +1,15 @@
+name = "_Starvew Valley"
+description = "Adds new veggies and recipes"
+author = "Red Tarantula"
+version = "0.1" 
+
+forumthread = ""
+
+api_version = 10
+
+dst_compatible = true
+
+all_clients_require_mod = true 
+
+icon_atlas = "modicon.xml"
+icon = "modicon.tex"
---
+++ Mod/modmain.lua
@@ -0,0 +1,9 @@
+PrefabFiles = {
+	"starvew_veggies"
+	}
+
+local G = GLOBAL
+local require = GLOBAL.require
+local STRINGS = GLOBAL.STRINGS
+local RECIPETABS = GLOBAL.RECIPETABS
+local TUNING = GLOBAL.TUNING
